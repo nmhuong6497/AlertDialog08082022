b30abeafb172b683ad1f15d5ba60648a97b6c30a
NMH <nm.huong6497@gmail.com>
2022-10-03 20:48:48 +0700

    Tính năng đơn giản

---
+++ app/src/main/java/com/huong/alertdialog08082022/MainActivity.java
@@ -0,0 +1,50 @@
+package com.huong.alertdialog08082022;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnAlertDialog, btnDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAlertDialog = findViewById(R.id.button_alert_dialog);
+        btnDialog = findViewById(R.id.button_dialog);
+
+        btnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Thông báo");
+                builder.setMessage("Đã có bản cập nhật mới");
+                builder.setIcon(android.R.drawable.star_on);
+                builder.setCancelable(false);
+
+                builder.setPositiveButton("Cập nhật", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
+    }
+}
