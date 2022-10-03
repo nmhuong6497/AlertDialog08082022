72423e97203870f3fb2ed14229495b3378ea4068
NMH <nm.huong6497@gmail.com>
2022-10-03 21:36:41 +0700

    Giao diện Dialog

--- app/src/main/java/com/huong/alertdialog08082022/MainActivity.java
+++ app/src/main/java/com/huong/alertdialog08082022/MainActivity.java
@@ -2,10 +2,15 @@ package com.huong.alertdialog08082022;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,6 +49,24 @@ public class MainActivity extends AppCompatActivity {
                 });
 
                 builder.show();
+            }
+        });
+
+        btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.layout_picker_color_dialog);
+
+                Window window = dialog.getWindow();
+
+                if (window != null) {
+                    window.setGravity(Gravity.BOTTOM);
+                    window.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+                }
+
+                dialog.show();
             }
         });
     }
